ffc951e4166efcd77340178c70a6816c22f80c79
AnnaMirana <annamiranarahanitriniaina@gmail.com>
2025-11-26 14:33:46 +0300

    feat: sprint 3 bis

--- src/com/mhframework/handler/controller/ClassMethod.java
+++ src/com/mhframework/handler/controller/ClassMethod.java
@@ -1,15 +1,19 @@
 package com.mhframework.handler.controller;
 
 import java.lang.reflect.Method;
+import java.util.regex.Matcher;
 
 public class ClassMethod {
 
     private Class<?> cls;
     private Method method;
+    private String url;
+    private Matcher matcher = null;
 
-    public ClassMethod(Class<?> cls, Method method) {
+    public ClassMethod(Class<?> cls, Method method, String url) {
         this.cls = cls;
         this.method = method;
+        this.url = url;
     }
 
     public Class<?> getCls() {
@@ -26,6 +30,22 @@ public class ClassMethod {
 
     public void setMethod(Method method) {
         this.method = method;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Matcher getMatcher() {
+        return matcher;
+    }
+
+    public void setMatcher(Matcher matcher) {
+        this.matcher = matcher;
     }
 
 }
